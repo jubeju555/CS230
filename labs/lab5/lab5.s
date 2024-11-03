61f59605ae43039dd6c85cc1066d2c99c21744a5
judah <judahjosh@gmail.com>
2024-11-03 13:32:58 -0500

    new file:   labs/lab5/.vscode/settings.json 	modified:   labs/lab5/lab5.s 	modified:   labs/lab5/mud.cpp 	modified:   labs/lab5/mud.rooms

--- labs/lab5/lab5.s
+++ labs/lab5/lab5.s
@@ -5,30 +5,36 @@ new_line: .asciz "\n"
 .section .text
 .global look_at_room
 
-
 # compile: riscv64-unknown-linux-gnu-g++ -o lab5 lab5.s mud.cpp
-# run ./mylab mud.rooms
+# run ./lab5 mud.rooms
 look_at_room:
     #  ...
+addi sp, sp, -8
+sd ra, 0(sp)
+
 ld a1, 0(a0) # Load the title
 ld a2, 8(a0) # Load the description
-
 la a0, exit_string
 call printf
 
 ret
 
-printf:
-    la      a0, exit_string       # Load the address of exit_string into a0
-    ld      a1, 0(a0)             # Load the title (assuming title is at offset 0)
-    ld      a2, 8(a0)             # Assuming the description field is at offset 8
-    call    printf
+la a0, new_line
+call printf
 
+li t0, 0
+
+# printf:
+#     la      a0, exit_string       # Load the address of exit_string into a0
+#     ld      a1, 0(a0)             # Load the title (assuming title is at offset 0)
+#     ld      a2, 8(a0)             # Assuming the description field is at offset 8
+#     call    printf
 
 look_at_all_rooms:
 li t0, 0
 mv t1, a1
 
+  
 
 1:
 bge t0, t1, loop_end
@@ -57,3 +63,6 @@ beq t1, t2, return_null
 slli t1, t1, 3
 add a0, a0, t1
 jr ra
+
+
+  
